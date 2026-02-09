58d120f5a0d030eb636ec0016be43d73dbec55c7
moaeydsede <moaeydsede98@gmail.com>
2026-02-09 16:42:33 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// ضع إعدادات Firebase هنا (Project Settings -> Web App)
+export const FIREBASE_CONFIG = {
+  apiKey: "PUT_API_KEY",
+  authDomain: "PUT_PROJECT.firebaseapp.com",
+  projectId: "PUT_PROJECT_ID",
+  // storageBucket: "PUT_PROJECT.appspot.com",
+  // messagingSenderId: "SENDER_ID",
+  // appId: "APP_ID"
+};
+
+// Tenant/Namespace (يمكن تغييره لاحقًا)
+export const TENANT_ID = "Main";
